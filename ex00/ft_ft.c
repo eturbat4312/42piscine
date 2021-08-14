897c73ce7f8aed8a1afacc2bcfda35327e9cc3a3
eturbat4312 <88346912+eturbat4312@users.noreply.github.com>
2021-08-14 10:29:42 +0200

    Add files via upload

---
+++ ex00/ft_ft.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+int	main(void)
+{
+	int	a;
+	int *nbr;
+
+	a = 21;
+	nbr = &a;
+	printf("%d\n", *nbr);
+	ft_ft(nbr);
+	printf("%d\n", *nbr);
+	return (0);
+}
